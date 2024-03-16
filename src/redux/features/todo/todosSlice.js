f3f870f31cf947dca649dfd591c2858cd0c1a83d
tonquocviet1 <viet.ton@huuk.social>
2024-03-16 23:50:47 +0700

    init Commit Setup

---
+++ src/redux/features/todo/todosSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export const counter = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    reset: () => initialState,
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+    decrementByAmount: (state, action) => {
+      state.value -= action.payload;
+    },
+  },
+});
+
+export const {
+  increment,
+  incrementByAmount,
+  decrement,
+  decrementByAmount,
+  reset,
+} = counter.actions;
+export default counter.reducer;
